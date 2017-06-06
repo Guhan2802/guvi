728c59a50026e242f697caf949d86eafcea64662
Guhan2802 <k.guhan1997@gmail.com>
2017-06-06 14:32:21 +0530

    Create number.cpp

---
+++ number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+	using namespace std;
+  int a;
+  cout<<"enter the number";
+  cin>>a;
+  if(a>0)
+  {
+    cout<<"negative number";
+  }
+  else if(a<0)
+  {
+    cout<<"positive number";
+  }
+  else
+    cout<<"zero";
+}
